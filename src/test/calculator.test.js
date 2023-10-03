042904886d9c05c16545a17b8b6bc0b57a0a710f
woojung007 <eeeeve007@gmail.com>
2023-10-03 18:05:51 +0900

    🧪 에러 테스트 하기

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -28,6 +28,12 @@ describe("calculator", () => {
     expect(cal.value).toBe(3);
   });
 
+  it("add should throw an error if value is greater than 100", () => {
+    expect(() => {
+      cal.add(101);
+    }).toThrow("Value can not be greater than 100");
+  });
+
   it("subtracts", () => {
     cal.subtract(1);
     expect(cal.value).toBe(-1);
