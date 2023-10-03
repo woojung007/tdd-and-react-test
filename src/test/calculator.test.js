13eaf3da85f154c2dd7c628a6764d8dbe5cb9196
woojung007 <eeeeve007@gmail.com>
2023-10-03 16:41:03 +0900

    ✅ feat: calculator 테스트 코드 작성

---
+++ src/test/calculator.test.js
@@ -0,0 +1,60 @@
+const Calculator = require("../calculator");
+
+describe("calculator", () => {
+  let cal;
+
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets", () => {
+    cal.set(3);
+    expect(cal.value).toBe(3);
+  });
+
+  it("clear", () => {
+    cal.set(3);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("add", () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it("subtracts", () => {
+    cal.subtract(1);
+    expect(cal.value).toBe(-1);
+  });
+
+  it("multiplies", () => {
+    cal.set(3);
+    cal.multiply(4);
+    expect(cal.value).toBe(12);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
