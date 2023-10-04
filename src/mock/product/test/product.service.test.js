f7735451755b94c5d56c6aa2cce312c6c17d4aac
woojung007 <eeeeve007@gmail.com>
2023-10-04 20:24:45 +0900

    💡 given, when, then

--- src/mock/product/test/product.service.test.js
+++ src/mock/product/test/product.service.test.js
@@ -5,12 +5,15 @@ describe("ProductService - Stub", () => {
   let productService;
 
   beforeEach(() => {
+    // Given, Arrange
     productService = new ProductService(new StubProductClient());
   });
 
   it("should filter out only available items.", async () => {
+    // When, Act
     const items = await productService.fetchAvailableItems();
 
+    // Then, Assert
     expect(items).toEqual([{ item: "🥛", available: true }]);
     expect(items.length).toBe(1);
   });
